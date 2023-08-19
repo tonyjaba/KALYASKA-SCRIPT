614a88b8b162db67b2d174d35575b3548d4a2598
tonyjaba <shamorod@mail.ru>
2023-08-20 01:03:09 +0300

    Added reg_exp

--- main.py
+++ main.py
@@ -2,22 +2,37 @@ import time
 import webbrowser
 import requests
 import winsound
+import re
 
+with open("reg_exp.txt", "r", encoding="utf-8") as f:
+    reg_exp = f.read()
 board = input('Введите доску(например news):\n')
+
+
 def get_thread():
     try:
-        r = requests.get('https://2ch.hk/'+board+'/catalog_num.json').json()
-        return r['threads'][0]['num']
+        r = requests.get('https://2ch.hk/' + board + '/catalog_num.json').json()
+        return r['threads'][0]
     except Exception as Ex:
         print('Ошибка соединения!(проверьте интернет-соединение и правильность ввода доски):\n' + Ex)
 
 
 current_thread = get_thread()
 while True:
-    if current_thread == get_thread():
+    new_thread = get_thread()
+    if current_thread['num'] == new_thread['num']:
         print('Нет новых тредов')
     else:
-        webbrowser.open('https://2ch.hk/'+board+'/res/' + str(get_thread()) + '.html', new=2, autoraise=True)
-        winsound.PlaySound("nya.wav", 0)
-        current_thread = get_thread()
+        if reg_exp:
+            if re.findall(reg_exp, new_thread['comment']):
+                print(re.findall(reg_exp, new_thread['comment']))
+                webbrowser.open('https://2ch.hk/' + board + '/res/' + str(new_thread['num']) + '.html', new=2,
+                                autoraise=True)
+                winsound.PlaySound("nya.wav", 0)
+                current_thread = new_thread
+        else:
+            webbrowser.open('https://2ch.hk/' + board + '/res/' + str(new_thread['num']) + '.html', new=2,
+                            autoraise=True)
+            winsound.PlaySound("nya.wav", 0)
+            current_thread = new_thread
     time.sleep(3)
